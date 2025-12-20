193c431b081ae4f21ba5223f777d87f99ac8b293
Emmely Gabriela <gabyintriago07@gmail.com>
2025-12-19 22:31:52 -0500

    OneToMany y ManyToOne

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn
+} from "typeorm";
+import { Product } from "./product.entity";
 
 @Entity()
 export class ProductImage {
@@ -8,4 +14,10 @@ export class ProductImage {
 
     @Column('text')
     url: string;
+
+    @ManyToOne(
+        () => Product,
+        product => product.images
+    )
+    product: Product;
 }
